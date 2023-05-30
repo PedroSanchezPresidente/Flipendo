956cd45692535018dae5edebcead934be697a24b
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2023-05-30 19:43:42 +0200

    Añadido comando ping

--- Flipendo/Flipendo/Flipendo.py
+++ Flipendo/Flipendo/Flipendo.py
@@ -1,8 +1,16 @@
+﻿import datetime
+from time import timezone
+from xml.etree.ElementTree import tostring
+
 import discord
+from datetime import date, tzinfo
 
 
 #Example code
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents = intents)
 
 @client.event
 async def on_ready():
@@ -14,6 +22,19 @@ async def on_message(message):
         return
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+        await message.channel.send('Hello!👍')
+
+    if message.content.startswith('$pin2'):
+        time = datetime.datetime.utcnow()
+        mtime = message.created_at
+        mtime = mtime.replace(tzinfo = None)
+
+        miliseconds = round((time - mtime).total_seconds() * 1000)
+        seconds = int(miliseconds / 1000)
+        miliseconds = miliseconds % 1000
+        
+        apiLatency = int((float(client.latency) * 1000) % 1000)
+
+        await message.channel.send('🏓Latency is ' + str(seconds) + 's ' + str(miliseconds) + 'ms. API Latency is '+ str(apiLatency) + 'ms')
 
 client.run('your token here')
