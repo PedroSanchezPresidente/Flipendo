40ac28633cc94c47471bd73c504cc9591f636cc1
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2023-08-17 23:42:35 +0200

    comandos take y change

--- Flipendo/Flipendo/Flipendo.py
+++ Flipendo/Flipendo/Flipendo.py
@@ -36,8 +36,7 @@ async def on_message(message):
 
         #Comando give
         elif message.content.startswith('!give'):
-            #await message.channel.send('Todabía está en desarrollo')
-            words = message.content.split(maxsplit=3)
+            words = message.content.split(maxsplit=2)
             if(len(words) >= 3):
                 roleName = words[1]
                 role = discord.utils.get(message.guild.roles, name=roleName)
@@ -45,22 +44,70 @@ async def on_message(message):
                     memberName = words[2]
                     member = discord.utils.get(message.guild.members, name=memberName)
                     if(member != None):
-                        await member.add_roles(role, reason=None, atomic=True)
-                        await message.channel.send(memberName + ' ha sido añadido a ' + roleName + '.')
+                        if(member in role.members):
+                            await message.channel.send('El miembro ' + memberName+ ' ya esta en ese rol.')
+                        else:
+                            await member.add_roles(role, reason=None, atomic=True)
+                            await message.channel.send(memberName + ' ha sido añadido a ' + roleName + '.')
                     else:
                         await message.channel.send('No se encrontró el miembro ' + memberName)
                 else:
-                    await message.channel.send('No se encrontró el role ' + roleName)
+                    await message.channel.send('No se encrontró el rol ' + roleName)
             else:
                 await message.channel.send('Se necesita especificar rol y miembro')
             
         #Comando take
         elif message.content.startswith('!take'):
-            await message.channel.send('Todabía está en desarrollo')
+            words = message.content.split(maxsplit=2)
+            if(len(words) >= 3):
+                roleName = words[1]
+                role = discord.utils.get(message.guild.roles, name=roleName)
+                if(role != None):
+                    memberName = words[2]
+                    member = discord.utils.get(message.guild.members, name=memberName)
+                    if(member != None):
+                        if(member in role.members):
+                            await member.remove_roles(role, reason=None, atomic=True)
+                            await message.channel.send(memberName + ' ya no tiene el rol de ' + roleName + '.')
+                        else:
+                            await message.channel.send('El miembro ' + memberName+ ' no esta en ese rol.')
+                    else:
+                        await message.channel.send('No se encrontró el miembro ' + memberName)
+                else:
+                    await message.channel.send('No se encrontró el rol ' + roleName)
+            else:
+                await message.channel.send('Se necesita especificar rol y miembro')
 
         #Comando change
         elif message.content.startswith('!change'):
-            await message.channel.send('Todabía está en desarrollo')
+            words = message.content.split(maxsplit=3)
+            if(len(words) >= 4):
+                roleName1 = words[1]
+                role1 = discord.utils.get(message.guild.roles, name=roleName1)
+                if(role1 != None):
+                    roleName2 = words[2]
+                    role2 = discord.utils.get(message.guild.roles, name=roleName2)
+                    if(role2 != None):
+                        memberName = words[3]
+                        member = discord.utils.get(message.guild.members, name=memberName)
+                        if(member != None):
+                            if(member in role1.members):
+                                if(member in role2.members):
+                                    await message.channel.send('El miembro ' + memberName+ ' ya esta en ese rol.')
+                                else:
+                                    await member.remove_roles(role1, reason=None, atomic=True)
+                                    await member.add_roles(role2, reason=None, atomic=True)
+                                    await message.channel.send(memberName + ' se le ha cambiado el rol de ' + roleName1 + ' a ' + roleName2+'.')
+                            else:
+                                await message.channel.send('El miembro ' + memberName+ ' no esta en ese rol.')
+                        else:
+                            await message.channel.send('No se encrontró el miembro ' + memberName)
+                    else:
+                        await message.channel.send('No se encrontró el rol ' + roleName2)
+                else:
+                    await message.channel.send('No se encrontró el rol ' + roleName1)
+            else:
+                await message.channel.send('Se necesita especificar rol antiguo, rol nuevo y miembro')
 
         #En caso de no encontrar el comando
         else:
