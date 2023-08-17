c9fd772f0e256fddae92db15acea40c64571b7a5
PedroSanchezPresidente <pedro.sanchez.vela72@gmail.com>
2023-08-17 22:08:54 +0200

    comando give

--- Flipendo/Flipendo/Flipendo.py
+++ Flipendo/Flipendo/Flipendo.py
@@ -6,6 +6,7 @@ from FlipendoMessages import FlipendoMessages
 
 #Example code
 intents = discord.Intents.default()
+intents.members = True
 intents.message_content = True
 
 client = discord.Client(intents = intents)
@@ -30,19 +31,35 @@ async def on_message(message):
             await message.channel.send(FlipendoMessages.pingMessage(message.created_at, client.latency))
 
         #Comando help
-        if message.content.startswith('!help'):
+        elif message.content.startswith('!help'):
             await message.channel.send('Todabía está en desarrollo')
 
         #Comando give
-        if message.content.startswith('!give'):
-            await message.channel.send('Todabía está en desarrollo')
+        elif message.content.startswith('!give'):
+            #await message.channel.send('Todabía está en desarrollo')
+            words = message.content.split(maxsplit=3)
+            if(len(words) >= 3):
+                roleName = words[1]
+                role = discord.utils.get(message.guild.roles, name=roleName)
+                if(role != None):
+                    memberName = words[2]
+                    member = discord.utils.get(message.guild.members, name=memberName)
+                    if(member != None):
+                        await member.add_roles(role, reason=None, atomic=True)
+                        await message.channel.send(memberName + ' ha sido añadido a ' + roleName + '.')
+                    else:
+                        await message.channel.send('No se encrontró el miembro ' + memberName)
+                else:
+                    await message.channel.send('No se encrontró el role ' + roleName)
+            else:
+                await message.channel.send('Se necesita especificar rol y miembro')
             
         #Comando take
-        if message.content.startswith('!take'):
+        elif message.content.startswith('!take'):
             await message.channel.send('Todabía está en desarrollo')
 
         #Comando change
-        if message.content.startswith('!change'):
+        elif message.content.startswith('!change'):
             await message.channel.send('Todabía está en desarrollo')
 
         #En caso de no encontrar el comando
